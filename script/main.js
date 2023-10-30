17bbb2f23c1e5b2788fcbab9619052b1f192dc76
limyeeun <ifg.035@daum.net>
2023-10-30 13:00:28 +0900

    mobile navi js, style 수정중

--- script/main.js
+++ script/main.js
@@ -26,15 +26,19 @@ $(function(){
     // menu button 클릭시 nav를 on,off를 하면서
     // =를 x로, x를 =로 변경
     let isMobile =screen.availWidth > 320 && screen.availWidth <= 600;
-    let isClicked = false;
+    let isClicked = true;
     $(".mo-navbtn").on("click", function(){
         if(isMobile) {
             //눌렀을때 :(최초) =를 x로, 그러면서 nav를 보여주고
             // 한 번 더 클릭했을떄 : x를 =로 바꾸고,nav를 감추기
             if(isClicked){
                 $(".mo-navbtn span").text("close");
-                $(".nav").css("display","block")
-                
+                $(".nav").css("display","block");
+                isClicked = false;
+            } else{
+                $(".mo-navbtn span").text("menu");
+                $(".nav").css("display","none");
+                isClicked = true;
             }
         }
     })
